5f6661210e7fa95d2464041e980c30040489123b
mb-tousif <meta.block.tousif@gmail.com>
2022-04-30 01:36:32 +0600

    log in page done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,17 +4,17 @@ import logo from '../../logo.png';
 
 const Header = () => {
     return (
-      <div className='md:flex bg-[#7130be] text-stone-50'>
-        <div className="flex p-4">
+      <div className='md:flex overflow-hidden w-full bg-[#7130be] text-stone-50'>
+        <div className="flex mx-auto my-auto p-4">
           <img src={logo} alt="LogoImage" className='w-12 h-10'/>
-          <h5 className='md:text-xl font-bold'>Neurosurgical Instruments</h5>
+          <h5 className='md:text-xl md:font-bold'>Neurosurgical Instruments</h5>
         </div>
-        <nav className='p-2 md:text-xl mx-auto my-auto font-semibold'>
-          <Link className='mr-4 justify-end' to="/home">Home</Link>
-          <Link className='mr-4 justify-end' to="/products">Products</Link>
-          <Link className='mr-4 justify-end' to="/inventory">Inventory</Link>
-          <Link className='mr-4 justify-end' to="/blog">Blogs</Link>
-          <Link className='mr-4 justify-end' to="/logIn">Log In</Link>
+        <nav className='p-2 md:text-xl mx-auto my-auto md:font-semibold'>
+          <Link className='mr-4' to="/home">Home</Link>
+          <Link className='mr-4' to="/products">Products</Link>
+          <Link className='mr-4' to="/inventory">Inventory</Link>
+          <Link className='mr-4' to="/blog">Blogs</Link>
+          <Link className='mr-4' to="/logIn">LogIn</Link>
         </nav>
       </div>
     );
